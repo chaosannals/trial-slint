f8db661baf3fea8aa172f650cd10a4d5adb4ae59
chaosannals <chaosannals@outlook.com>
2023-08-30 17:58:29 +0800

    save

--- rustdemo/app/src/main.rs
+++ rustdemo/app/src/main.rs
@@ -3,6 +3,11 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
+    ui.global::<Theme>().on_magic_operation(|value| {
+        eprintln!("magic operation input: {}", value);
+        value * 2
+    });
+
     let ui_handle = ui.as_weak();
     ui.on_request_increase_value(move || {
         let ui = ui_handle.unwrap();
