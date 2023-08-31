4c2428e5cfbe40416821a37c3706870f66a8c449
chaosannals <chaosannals@outlook.com>
2023-08-31 11:29:15 +0800

    save

--- rustdemo/app/src/main.rs
+++ rustdemo/app/src/main.rs
@@ -1,7 +1,26 @@
+use anyhow::Result;
+use slint::{SharedPixelBuffer, Image};
+
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
+// fn main() -> Result<(), slint::PlatformError> {
+fn main() -> Result<()> {
     let ui = AppWindow::new()?;
+
+    let mut image_buf = vec![];
+    let _ = ureq::get("https://picsum.photos/id/2/400/400.jpg")
+        .call()?
+        .into_reader()
+        .read_to_end(&mut image_buf)?;
+    let img = image::load_from_memory(&image_buf)?.to_rgba8();
+
+    ui.on_load_image(move || {
+        Image::from_rgba8(SharedPixelBuffer::clone_from_slice(
+            &img,
+            img.width(),
+            img.height(),
+        ))
+    });
 
     ui.global::<Theme>().on_magic_operation(|value| {
         eprintln!("magic operation input: {}", value);
@@ -14,5 +33,7 @@ fn main() -> Result<(), slint::PlatformError> {
         ui.set_counter(ui.get_counter() + 1);
     });
 
-    ui.run()
+    let _ = ui.run(); // 和 anyhow::Result 不兼容
+
+    Ok(())
 }
